9b06045541044e93f892d7325b989706a537fd2f
arthursiq5 <arthur.siq5@gmail.com>
2021-08-04 21:23:39 -0300

    Adicionadas imagens

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,40 @@
-console.log("entry app!!");
+import Phaser from "phaser";
+import sky from './assets/sky.png';
+import ground from './assets/platform.png';
+import star from './assets/star.png';
+import bomb from './assets/bomb.png';
+import dude from './assets/dude.png';
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', sky);
+    this.load.image('ground', ground);
+    this.load.image('star', star);
+    this.load.image('bomb', bomb);
+    this.load.spritesheet('dude', 
+        dude,
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+}
+
+function update ()
+{
+}
