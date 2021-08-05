452d5e2ea8e5f25651fbe6a7e165e536a80bb2d7
arthursiq5 <arthur.siq5@gmail.com>
2021-08-04 21:35:34 -0300

    Criadas plataformas iniciais

--- src/entry.js
+++ src/entry.js
@@ -9,6 +9,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -30,9 +37,18 @@ function preload ()
     );
 }
 
+var platforms;
+
 function create ()
 {
     this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
